c562922b5123238ab482de2979840763d129ce41
github_username <you@domain.com>
2023-02-12 00:03:02 +0600

    deposit total Amount

--- js/deposit.js
+++ js/deposit.js
@@ -2,8 +2,11 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // console.log('deposite Amount');
     const depositeField = document.getElementById('deposit-field');
     const depositValue = depositeField.value;
-    console.log(depositValue);
+    // console.log(depositValue);
     const depositeInput = document.getElementById('deposit-input');
-    console.log(depositeInput)
+    // console.log(depositeInput)
+    const depositTotal = depositeInput.innerText;
+    // console.log(depositTotal);
+    depositeInput.innerText = depositValue;
 
 })
