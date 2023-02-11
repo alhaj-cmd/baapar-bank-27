1a1c4decf1d2c69a779b2dc6268a53db44ca4838
github_username <you@domain.com>
2023-02-12 00:36:33 +0600

    deposite complete section

--- js/deposit.js
+++ js/deposit.js
@@ -2,11 +2,18 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // console.log('deposite Amount');
     const depositeField = document.getElementById('deposit-field');
     const depositValue = depositeField.value;
+    const newdepositValue =parseFloat(depositValue);
     // console.log(depositValue);
     const depositeInput = document.getElementById('deposit-input');
     // console.log(depositeInput)
     const depositTotal = depositeInput.innerText;
     // console.log(depositTotal);
-    depositeInput.innerText = depositValue;
+    const newdepositTotal =parseFloat(depositTotal);
+    // count deposit amount
+    const currentAmount = newdepositValue+newdepositTotal;
+    depositeInput.innerText = currentAmount;
+
+    // clear the deposit amount;
+    depositeField.value  = " ";
 
 })
