e4efe1150d1683af6d651d9457e4c58e61083122
github_username <you@domain.com>
2023-02-11 22:11:17 +0600

    third commit

---
+++ js/submit.js
@@ -0,0 +1,19 @@
+// console.log('hello');
+// step-1 add click event handler with button 
+
+document.getElementById('btn-submit').addEventListener('click',function (){
+    // console.log('This day is off!!');\
+    const emailField = document.getElementById('user-email');
+    const emailValue = emailField.value;
+    // step-3 get password
+    const passwordField = document.getElementById('user-password');
+    const passwordValue = passwordField.value;
+    // console.log(emailValue, passwordValue);
+    // step-4 verify emailField && passwordField and check wheather valid or not
+    if(emailValue ==='alhazsarkar55@gmail.com' && passwordValue ==='alhaj' ){
+       console.log('user is valid')
+    }
+    else{
+       console.log('user is not valid')
+    }
+})
