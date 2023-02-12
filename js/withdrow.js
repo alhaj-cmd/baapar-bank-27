e5001db592f20114dfd94f0455b3bf394fa422e2
github_username <you@domain.com>
2023-02-12 20:35:47 +0600

    withdrow amount added

--- js/withdrow.js
+++ js/withdrow.js
@@ -1,1 +1,20 @@
-console.log('withdrow your account');
+// console.log('withdrow your account');
+document.getElementById('btn-withdrow').addEventListener('click', function(){
+    // console.log('Baap er bank withdrow');
+    const withdrowField = document.getElementById('withdrow-field');
+    // console.log(withdrowField);
+    const withdrowValue = withdrowField.value;
+    // console.log(withdrowValue);
+    // withdrowField.value=withdrowAmount;
+    const withdrowAmount =parseFloat(withdrowValue);
+    // withdrowField.value=withdrowAmount;
+
+    const withdrowInput = document.getElementById('withdrow-input');
+    const withdrowText = withdrowInput.innerText;
+    // withdrowInput.innerText= withdrowTextAmount;
+    const withdrowTextAmount = parseFloat(withdrowText);
+    // console.log(withdrowAmount,withdrowTextAmount);
+    const newwithdrowAmount = withdrowAmount + withdrowTextAmount;
+    withdrowInput.innerText = newwithdrowAmount;
+    withdrowField.value = " ";
+})
