25dc533b7e644499c7e1871d0e8a92c1e5a3e912
github_username <you@domain.com>
2023-02-12 17:15:56 +0600

    Total depostite amount

--- js/deposit.js
+++ js/deposit.js
@@ -1,19 +1,32 @@
-document.getElementById('btn-deposit').addEventListener('click', function(){
+document.getElementById('btn-deposit').addEventListener('click', function () {
     // console.log('deposite Amount');
     const depositeField = document.getElementById('deposit-field');
     const depositValue = depositeField.value;
-    const newdepositValue =parseFloat(depositValue);
+    const newdepositValue = parseFloat(depositValue);
     // console.log(depositValue);
     const depositeInput = document.getElementById('deposit-input');
     // console.log(depositeInput)
     const depositTotal = depositeInput.innerText;
     // console.log(depositTotal);
-    const newdepositTotal =parseFloat(depositTotal);
+    const newdepositTotal = parseFloat(depositTotal);
+
     // count deposit amount
-    const currentAmount = newdepositValue+newdepositTotal;
+    const currentAmount = newdepositValue + newdepositTotal;
     depositeInput.innerText = currentAmount;
 
+
+    // get balance current total
+    const balanceInputs = document.getElementById('balance-input');
+    const balanceAmount = balanceInputs.innerText;
+    const previousBalance = parseFloat(balanceAmount);
+
+    const currentBalanceTotal = previousBalance + newdepositTotal;
+    balanceInputs.innerText = currentBalanceTotal;
+
+
+
+
     // clear the deposit amount;
-    depositeField.value  = " ";
+    depositeField.value = " ";
 
 })
